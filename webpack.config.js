f85d98c783fafeb89bad7a33cba68eec1689c7ef
Nahom Assefa <nzenebe@gmail.com>
2022-02-15 12:45:19 -0600

    progress separating concerns

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 const path = require("path");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const webpack = require("webpack");
 
-
-
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js',
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        })
-    ],
-    mode: 'development',
+  entry: "./assets/js/script.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.bundle.js",
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    }),
+  ],
+  mode: "development",
 };
